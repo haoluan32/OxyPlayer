5bae46add14c70fd31e1f3e7a1f8b93affe3a722
haoluan <haoluanchat@outlook.com>
2026-07-31 13:19:54 +0800

    将musicsh类中的部分函数移动到Toolkit中

---
+++ OxyPlayer/Toolkit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OxyPlayer
+{
+    internal class Toolkit
+    {
+        [DllImport("kernel32.dll")]
+        public static extern uint GetTickCount();
+        static public void Delay(uint ms)
+        {
+            uint start = GetTickCount();
+            while (GetTickCount() - start < ms)
+            {
+                System.Windows.Forms.Application.DoEvents();
+            }
+        }
+
+        static public int GetRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+    }
+}
